1b77114a7c5df285ca92b2b9aa46cc0149ac6a03
Shenghao1993 <wangshenghao1993@gmail.com>
2019-11-18 01:18:02 +0800

    Refine feature engineering

--- helpers.py
+++ helpers.py
@@ -1,79 +1,120 @@
 import numpy as np
 import pandas as pd
+import re
 import time
-from sklearn.ensemble import RandomForestRegressor
+from sklearn import preprocessing
 from sklearn.model_selection import RandomizedSearchCV, GridSearchCV
 from sklearn.metrics import accuracy_score, precision_score, recall_score, roc_curve, auc, confusion_matrix
 
 
 def extract_title(full_name):
-    names = full_name.split(' ')
-    for name in names:
-        if name[-1] == '.':
-            return name[:-1]
+    title_search = re.search(' ([A-Za-z]+)\.', full_name)
+    if title_search:
+        title = title_search.group(1)
+        title.replace('Ms', 'Miss')
+        # Normalise any title in French
+        title.replace('Mlle', 'Miss')
+        title.replace('Mme', 'Mrs')
+        return title
     return ''
+
+
+def normalize_room_no(deck, room_no, max_room_no_dict):
+    return room_no / max_room_no_dict.get(deck)
 
 
 def engineer_features(df):
     '''Process features '''
-    # Cabin: Generate Deck, Room from Cabin
-    df['Deck'] = df['Cabin'].str.slice(0,1)
-    df['Deck'].fillna('U', inplace=True)
-    df['Room'] = df['Cabin'].str.slice(1,5).str.extract("([0-9]+)", expand=False).astype("float")
-    df['Room'].fillna(df["Room"].mean(), inplace=True)
-    
-    # Embarked: Fill in missing values with the most popular port
-    df['Embarked'].fillna('S', inplace=True)
-    
+    # Map Sex
+    df['Sex'] = df['Sex'].map( {'female': 0, 'male': 1} ).astype(int)
+
+    # Fill in missing Age value
+    age_avg = df['Age'].mean()
+    age_std = df['Age'].std()
+    age_null_count = df['Age'].isnull().sum()
+    age_null_random_list = np.random.randint(age_avg - age_std, age_avg + age_std, size=age_null_count)
+    df['CategoricalAge'] = df['Age']
+    df['CategoricalAge'][np.isnan(df['CategoricalAge'])] = age_null_random_list
+
+    # Map Age
+    df.loc[ df['CategoricalAge'] <= 16, 'CategoricalAge'] = 0
+    df.loc[(df['CategoricalAge'] > 16) & (df['CategoricalAge'] <= 32), 'CategoricalAge'] = 1
+    df.loc[(df['CategoricalAge'] > 32) & (df['CategoricalAge'] <= 48), 'CategoricalAge'] = 2
+    df.loc[(df['CategoricalAge'] > 48) & (df['CategoricalAge'] <= 64), 'CategoricalAge'] = 3
+    df.loc[ df['CategoricalAge'] > 64, 'CategoricalAge'] = 4
+    df['CategoricalAge'] = df['CategoricalAge'].astype(int)
+
+    # Create FamilySize feature
+    df['FamilySize'] = df['SibSp'] + df['Parch'] + 1
+    min_max_scaler = preprocessing.MinMaxScaler()
+    df['NormalizedFamilySize'] = min_max_scaler.fit_transform(df[['FamilySize']].values)
+    df['IsAlone'] = 0
+    df.loc[df['FamilySize'] == 1, 'IsAlone'] = 1
+
     # Name: Generate Title
     df['Title'] = df['Name'].apply(extract_title)
     popular_titles = ['Mrs', 'Miss', 'Master', 'Mr']
-    df['Title'] = np.where(~df['Title'].isin(popular_titles), 'Other', df['Title'])
+    # Group all non-common titles into one single group "Rare"
+    df['Title'] = np.where(~df['Title'].isin(popular_titles), 'Rare', df['Title'])
+    df['NameLength'] = df['Name'].apply(len)
+    df['NormalizedNameLength'] = min_max_scaler.fit_transform(df[['NameLength']].values)
+
+    # Mapping titles
+    title_mapping = {"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Rare": 5}
+    df['Title'] = df['Title'].map(title_mapping)
+    df['Title'] = df['Title'].fillna(0)
+
+    # Mapping Fare
+    df.loc[ df['Fare'] <= 7.91, 'Fare'] = 0
+    df.loc[(df['Fare'] > 7.91) & (df['Fare'] <= 14.454), 'Fare'] = 1
+    df.loc[(df['Fare'] > 14.454) & (df['Fare'] <= 31), 'Fare']   = 2
+    df.loc[ df['Fare'] > 31, 'Fare'] = 3
+    df['Fare'] = df['Fare'].astype(int)
+
+    # Cabin: Generate Deck, Room from Cabin
+    df['HasCabin'] = df["Cabin"].apply(lambda x: 0 if type(x) == float else 1)
+    df['Deck'] = df['Cabin'].str.slice(0,1)
+    df['Deck'].fillna('U', inplace=True)
+    df['Room'] = df['Cabin'].str.slice(1,5).str.extract("([0-9]+)", expand=False).astype("float")
+    max_room_no_df = df[["Deck", "Room"]].groupby(['Deck'], as_index=False).max()
+    max_room_no_dict = max_room_no_df.set_index('Deck').to_dict().get('Room')
+    print(max_room_no_dict)
+    df['NormalizedRoomNo'] = df.apply(lambda x: normalize_room_no(x.Deck, x.Room,
+                                                                  max_room_no_dict), axis=1)
+    df['NormalizedRoomNo'].fillna(df['NormalizedRoomNo'].mean(), inplace=True)
     
+    # Embarked: Fill in missing values with the most popular port
+    df['Embarked'].fillna('S', inplace=True)
+    df['Embarked'] = df['Embarked'].map( {'S': 0, 'C': 1, 'Q': 2} ).astype(int)
+
     return df
 
 
-def preprocess(training_data, test_data):
-    # Combine training and test datasets to have more training data with valid age values
-    train_df = pd.read_csv(training_data)
-    test_df = pd.read_csv(test_data)
-    combined_df = pd.concat([train_df, test_df])
-    selected_features = ['PassengerId', 'Age', 'Deck', 'Room', 'Embarked', 'Fare', 'Title',
-                         'Parch', 'Pclass', 'Sex', 'SibSp', 'Survived']
-    # One-hot encode the engineered features
-    processed_combined_df = pd.get_dummies(engineer_features(combined_df)[selected_features])
-    print(processed_combined_df.columns)
+def preprocess(train_data_path, test_data_path):
+    # Load training and test datasets
+    train_df = pd.read_csv(train_data_path)
+    test_df = pd.read_csv(test_data_path)
+    processed_dfs = []
+    drop_cols = ['PassengerId', 'NameLength', 'SibSp', 'Parch', 'Ticket',
+                 'FamilySize', 'Cabin', 'Deck', 'Room']
+    for df in [train_df, test_df]:
+        # Fill in missing Fare value with the median of training set.
+        df['Fare'] = df['Fare'].fillna(train_df['Fare'].median())
 
-    # Filter out unwanted features
-    features = list(processed_combined_df.columns)
-    for feature in ['PassengerId', 'Age', 'Survived']:
-        features.remove(feature)
-
-    # Fill in missing value with the average Fare of class 3 
-    avg_class3_fare = processed_combined_df[processed_combined_df['Pclass'] == 3]['Fare'].mean()
-    processed_combined_df['Fare'].fillna(avg_class3_fare, inplace=True)
-    null_age_df = processed_combined_df[processed_combined_df['Age'].isnull()]
-    not_null_age_df = processed_combined_df[processed_combined_df.Age.notnull()]
-    print("Number of passengers with age data: %s" %not_null_age_df.shape[0])
-    print("Number of passengers with no age data: %s" %null_age_df.shape[0])
-
-    # Fit Random Forest model to predict missing age
-    age_rf = RandomForestRegressor(n_estimators=1000, random_state=42)
-    age_rf.fit(not_null_age_df[features], not_null_age_df['Age'])
-    null_age_df.loc[:, 'Predicted_Age'] = age_rf.predict(null_age_df[features])
-    not_null_age_df['Predicted_Age'] = not_null_age_df['Age'] 
-
-    # Combine data with predicted age
-    age_predicted_df = pd.concat([not_null_age_df, null_age_df], sort=False)
-    age_predicted_df = pd.merge(age_predicted_df, combined_df[['PassengerId', 'Name']],
-                                how='left', on=['PassengerId']) 
-    return age_predicted_df, features
+        # One-hot encode the categorical features
+        encoded_df = pd.get_dummies(engineer_features(df),
+                                    columns=['Pclass', 'CategoricalAge', 'Fare', 'Embarked', 'Title'])
+        print("One-hot encoded columns: ", encoded_df.columns)
+        encoded_df = encoded_df.drop(drop_cols, axis = 1)
+        processed_dfs.append(encoded_df)
+    return processed_dfs
 
 
 def select_params(model, param_grid, X_train, y_train, nfolds, grid_search=True):
     # scores = ['accuracy', 'precision', 'recall']
     scores = ['accuracy']
     start_time = time.time()
+    best_score = {}
     for score in scores:
         print("# Tuning hyper-parameters for %s" % score)
         print()
@@ -91,12 +132,13 @@ def select_params(model, param_grid, X_train, y_train, nfolds, grid_search=True)
         print()
         means = clf.cv_results_['mean_test_score']
         stds = clf.cv_results_['std_test_score']
+        best_score[score] = max(means)
         for mean, std, params in zip(means, stds, clf.cv_results_['params']):
             print("%0.3f (+/-%0.03f) for %r"
                   % (mean, std * 2, params))
         print("Elapsed time: %s seconds" % round(time.time() - start_time, 4))
         print()
-    return clf
+    return clf, best_score
 
 
 def evaluate(model, features, y_true):
